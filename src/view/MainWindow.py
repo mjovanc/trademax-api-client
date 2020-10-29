7d6a186df7e6e8a6a37439c0dcc05e710a882f24
mjovanc <mjovanc@protonmail.com>
2020-10-29 19:21:43 +0100

    Changing to QStackedWidget

--- src/view/MainWindow.py
+++ src/view/MainWindow.py
@@ -1,13 +1,14 @@
 import sys
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QMessageBox
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QMessageBox, QWidget, QMainWindow, QStackedWidget
 
 from model.TrademaxAPI import TrademaxAPI
 
-# class AboutWidget(QWidget):
-#     def __init__(self, parent=None):
-#         super(AboutWidget, self).__init__(parent)
-#         uic.loadUi('view/about_widget.ui')
+
+class AboutWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi('view/about_widget.ui', self)
 
 
 class PurchaseOrderWidget(QListWidget):
@@ -16,44 +17,48 @@ class PurchaseOrderWidget(QListWidget):
         uic.loadUi('view/purchase_order_widget.ui')
 
 
-class PurchaseOrdersWidget(QListWidget):
-    def __init__(self, parent=None):
-        super(PurchaseOrdersWidget, self).__init__(parent)
-        uic.loadUi('view/purchase_orders_widget.ui')
-        self.trademax_api = TrademaxAPI()
-        self.set_intial_list_data()
-
-        self.itemClicked.connect(self.open_purchase_order)
-
-    def set_column_labels(self):
-        po = self.trademax_api.get_all_purchase_orders()
-        for key, value in po[0].items():
-            self.po_data2[str(key)] = []
-
-    def set_intial_list_data(self):
-        po = self.trademax_api.get_all_purchase_orders()
-        for p in po:
-            self.addItem(QListWidgetItem(p['purchase_order_id'], self))
-
-    def open_purchase_order(self, item):
-        purchase_order_id = item.text()
-
-
-
-
-
-class MainWindow(QtWidgets.QMainWindow):
+class PurchaseOrdersWidget(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        uic.loadUi('view/main_window.ui', self)
-        self.show()
-        self.btn_purchase_orders.clicked.connect(self.start_purchase_orders_widget)
+        uic.loadUi('view/purchase_orders_widget.ui', self)
+        # self.trademax_api = TrademaxAPI()
+        # self.purchase_orders = self.trademax_api.get_all_purchase_orders()
+        # self.set_intial_list_data()
+        # self.itemClicked.connect(self.open_purchase_order)
 
-    def start_purchase_orders_widget(self):
-        self.setWindowTitle(self.windowTitle() + ' - Inköpsordrar')
-        self.purchaseOrdersWidget = PurchaseOrdersWidget(self)
-        self.setCentralWidget(self.purchaseOrdersWidget)
-        self.show()
+    # def set_column_labels(self):
+    #     for key, value in self.purchase_orders[0].items():
+    #         self.po_data2[str(key)] = []
+    #
+    # def set_intial_list_data(self):
+    #    for p in self.purchase_orders:
+    #         self.purchase_orders_widget.addItem(QListWidgetItem(p['purchase_order_id'], self))
+    #
+    # def open_purchase_order(self, item):
+    #     purchase_order_id = item.text()
+    #     # change to another scene (class)
+
+
+class MainWindow(QStackedWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('view/main_view_stackedwidget.ui', self)
+
+        self.purchase_orders_widget = PurchaseOrdersWidget()
+        self.addWidget(self.purchase_orders_widget)
+
+        self.about_widget = AboutWidget()
+        self.addWidget(self.about_widget)
+
+        self.btn_purchase_orders.clicked.connect(self.go_to_first)
+        print(self.btn_purchase_orders)
+        self.btn_about.clicked.connect(self.go_to_second)
+
+    def go_to_first(self):
+        self.setCurrentIndex(1)
+
+    def go_to_second(self):
+        self.setCurrentIndex(2)
 
 
 app = QtWidgets.QApplication(sys.argv)
