73ada1fb81a8413b594b18bb2c3fea058d49f17e
mjovanc <mjovanc@protonmail.com>
2020-12-08 09:45:40 +0100

    Adding test cases

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,5 +1,7 @@
 import unittest
 
+from requests import HTTPError
+
 from src.model.trademax_api import TrademaxAPI
 
 
@@ -9,5 +11,14 @@ class APITest(unittest.TestCase):
         self.assertTrue(type(self.trademax_api.TOKEN) is str)
 
     def test_get_purchase_order(self):
-        pass
+        self.trademax_api = TrademaxAPI()
+
+        # Testing a wrong purchase order id
+        p1 = self.trademax_api.get_purchase_order('IOT-wrong-id')
+        self.assertRaises(p1, HTTPError)
+
+        # Testing a correct purchase order id
+        p2 = self.trademax_api.get_purchase_order('IOT1002674')
+        self.assertTrue(type(p2) is dict)
+
 
